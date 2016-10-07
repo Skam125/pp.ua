287dc3b3ba6c026145271b13ffff2cda84ec6474
Skam <Skam>
2016-10-07 11:55:09 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Skam</title>
+    <link href="css/style.css" type="text/css" rel="stylesheet">
+</head>
+<body>
+<div class="logo">
+    <a href="http://skam.pp.ua/"><img src="img/skam.png" width="500px"/></a>
+</div>
+</body>
+</html>
